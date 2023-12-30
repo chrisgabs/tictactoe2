5942c9367b22ee33b0d11e3d452d5f53ca8ec7f3
Christian Bayquen <cgabayquen@gmail.com>
2023-12-31 01:17:41 +0800

    fix: vite environment variables reference

--- src/lib/config/constants.js
+++ src/lib/config/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.API_ENDPOINT;
+export const API_BASE_URL = import.meta.env.API_ENDPOINT;
 // export const API_BASE_URL = '192.168.1.86';
 export const API_BASE_PORT = '8080';
